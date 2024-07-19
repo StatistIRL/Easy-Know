758918875d35363d7cee84a612f891f9edbf1a56
StatistIRL <vazsry7@gmail.com>
2024-07-19 20:17:03 +0600

    feat(admin): add CourseAdmin & CategoryAdmin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from courses.models import Category, Course
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["title", "level", "category"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "level"]
