4dc3043b243d96ba6affa36c71336eeb8ed102c0
StatistIRL <vazsry7@gmail.com>
2024-07-22 22:55:17 +0600

    feat(thumbnails): Add model Thumbnail

--- thumbnails/models.py
+++ thumbnails/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from .mixins import ModelDiffMixin
 
-# Create your models here.
+
+class Thumbnail(ModelDiffMixin, models.Model):
+    course = models.OneToOneField(
+        "courses.Course",
+        on_delete=models.CASCADE,
+        related_name="thumbnail",
+        blank=True,
+        null=True,
+    )
+    original = models.ImageField()
+    big = models.ImageField(blank=True, null=True)
+    medium = models.ImageField(blank=True, null=True)
+    small = models.ImageField(blank=True, null=True)
+
+    class Meta:
+        db_table = "thumbnail"
