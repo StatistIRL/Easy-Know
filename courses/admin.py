b4b769a1397abf39b4f7a54b176ba29ef9cdaa04
StatistIRL <vazsry7@gmail.com>
2024-07-22 23:04:31 +0600

    feat(thumbnails): Add Thumbnail model to admin panel & display it inline for courses

--- courses/admin.py
+++ courses/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
 from courses.models import Category, Course
+from thumbnails.models import Thumbnail
+
+
+class ThumbnailInline(admin.TabularInline):
+    model = Thumbnail
+    fields = ["original"]
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ["title", "level", "category"]
+    inlines = [ThumbnailInline]
 
 
 @admin.register(Category)
--- thumbnails/admin.py
+++ thumbnails/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from thumbnails.models import Thumbnail
+
 # Register your models here.
+
+
+@admin.register(Thumbnail)
+class ThumbnailAdmin(admin.ModelAdmin):
+    list_display = ["id", "original"]
